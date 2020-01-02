67f49911b8ec943ead29b2365cae0ebdb2688b3b
Jose Riego Valenzuela <jvalenzuela@indeed.com>
2020-01-02 18:53:37 +0000

    wip target bigquery fastsync

---
+++ pipelinewise/fastsync/commons/target_bigquery.py
@@ -0,0 +1,246 @@
+from typing import List
+
+from google.cloud import bigquery
+from google.cloud.bigquery.job import SourceFormat
+from google.cloud.bigquery import Dataset, WriteDisposition
+from google.cloud.bigquery import SchemaField
+from google.cloud.bigquery import LoadJobConfig
+from google.api_core import exceptions
+
+import time
+import os
+
+from . import utils
+
+class FastSyncTargetSnowflake:
+    EXTRACTED_AT_COLUMN = '_sdc_extracted_at'
+    BATCHED_AT_COLUMN = '_sdc_batched_at'
+    DELETED_AT_COLUMN = '_sdc_deleted_at'
+
+    def __init__(self, connection_config, transformation_config=None):
+        self.connection_config = connection_config
+        self.transformation_config = transformation_config
+
+    def open_connection(self):
+        project_id = self.connection_config['project_id']
+        return bigquery.Client(project=project_id)
+
+    def query(self, query, params=[]):
+        def to_query_parameter(value):
+            if isinstance(value, int):
+                value_type = "INT64"
+            elif isinstance(value, float):
+                value_type = "NUMERIC"
+            #TODO: repeated float here and in target
+            elif isinstance(value, float):
+                value_type = "FLOAT64"
+            elif isinstance(value, bool):
+                value_type = "BOOL"
+            else:
+                value_type = "STRING"
+            return bigquery.ScalarQueryParameter(None, value_type, value)
+
+        job_config = bigquery.QueryJobConfig()
+        query_params = [to_query_parameter(p) for p in params]
+        job_config.query_parameters = query_params
+
+        queries = []
+        if type(query) is list:
+            queries.extend(query)
+        else:
+            queries = [query]
+
+        client = self.open_connection()
+        logger.info("TARGET_BIGQUERY - Running query: {}".format(query))
+        query_job = client.query(';\n'.join(queries), job_config=job_config)
+        query_job.result()
+
+        return query_job
+
+
+    def create_schema(self, schema_name):
+        temp_schema = self.connection_config.get('temp_schema', self.schema_name)
+        schema_rows = 0
+
+        for schema in set([schema_name, temp_schema]):
+                schema_rows = self.query(
+                    'SELECT LOWER(schema_name) schema_name FROM INFORMATION_SCHEMA.SCHEMATA WHERE LOWER(schema_name) = ?',
+                    (schema.lower(),)
+                )
+
+            if schema_rows.result().total_rows == 0:
+                logger.info("Schema '{}' does not exist. Creating...".format(schema))
+                client = self.open_connection()
+                dataset = client.create_dataset(schema)
+
+    def drop_table(self, target_schema, table_name, is_temporary=False):
+        table_dict = utils.tablename_to_dict(table_name)
+        target_table = table_dict.get('table_name') if not is_temporary else table_dict.get('temp_table_name')
+
+        sql = "DROP TABLE IF EXISTS {}.{}".format(target_schema, target_table)
+        self.query(sql)
+
+    def create_table(self, target_schema: str, table_name: str, columns: List[str], primary_key: str,
+                     is_temporary: bool = False, sort_columns = False):
+
+        table_dict = utils.tablename_to_dict(table_name)
+        target_table = table_dict.get('table_name') if not is_temporary else table_dict.get('temp_table_name')
+
+        # skip the EXTRACTED, BATCHED and DELETED columns in case they exist because they gonna be added later
+        columns = [c for c in columns if not (c.startswith(self.EXTRACTED_AT_COLUMN) or
+                                              c.startswith(self.BATCHED_AT_COLUMN) or
+                                              c.startswith(self.DELETED_AT_COLUMN))]
+
+        columns += [f'{self.EXTRACTED_AT_COLUMN} TIMESTAMP',
+                    f'{self.BATCHED_AT_COLUMN} TIMESTAMP',
+                    f'{self.DELETED_AT_COLUMN} STRING'
+                    ]
+
+        sql = f"""CREATE OR REPLACE TABLE {target_schema}.{target_table} (
+        {','.join(columns)})
+        """
+
+        self.query(sql)
+
+    def copy_to_table(self, file_name, target_schema, table_name, is_temporary, skip_csv_header=False):
+        utils.log("SNOWFLAKE - Loading {} into Snowflake...".format(s3_key))
+        table_dict = utils.tablename_to_dict(table_name)
+        target_table = table_dict.get('table_name') if not is_temporary else table_dict.get('temp_table_name')
+
+        aws_access_key_id = self.connection_config['aws_access_key_id']
+        aws_secret_access_key = self.connection_config['aws_secret_access_key']
+        bucket = self.connection_config['s3_bucket']
+
+        master_key = self.connection_config.get('client_side_encryption_master_key', '')
+        if master_key != '':
+            sql = """COPY INTO {}.{} FROM @{}/{}
+                FILE_FORMAT = (type='CSV' escape='\\x1e' escape_unenclosed_field='\\x1e' 
+                field_optionally_enclosed_by='\"' skip_header={} COMPRESSION='GZIP') 
+            """.format(
+                target_schema,
+                target_table,
+                self.connection_config['stage'],
+                s3_key,
+                int(skip_csv_header),
+            )
+        else:
+            sql = """COPY INTO {}.{} FROM 's3://{}/{}'
+                CREDENTIALS = (aws_key_id='{}' aws_secret_key='{}')
+                FILE_FORMAT = (type='CSV' escape='\\x1e' escape_unenclosed_field='\\x1e' 
+                field_optionally_enclosed_by='\"' skip_header={} COMPRESSION='GZIP')
+            """.format(
+                target_schema,
+                target_table,
+                bucket,
+                s3_key,
+                aws_access_key_id,
+                aws_secret_access_key,
+                int(skip_csv_header),
+            )
+
+        self.query(sql)
+
+        utils.log("SNOWFLAKE - Deleting {} from S3...".format(s3_key))
+        self.s3.delete_object(Bucket=bucket, Key=s3_key)
+
+    # grant_... functions are common functions called by utils.py: grant_privilege function
+    # "to_group" is not used here but exists for compatibility reasons with other database types
+    # "to_group" is for databases that can grant to users and groups separately like Amazon Redshift
+    def grant_select_on_table(self, target_schema, table_name, role, is_temporary, to_group=False):
+        # Grant role is not mandatory parameter, do nothing if not specified
+        if role:
+            table_dict = utils.tablename_to_dict(table_name)
+            target_table = table_dict.get('table_name') if not is_temporary else table_dict.get('temp_table_name')
+            sql = "GRANT SELECT ON {}.{} TO ROLE {}".format(target_schema, target_table, role)
+            self.query(sql)
+
+    def grant_usage_on_schema(self, target_schema, role, to_group=False):
+        # Grant role is not mandatory parameter, do nothing if not specified
+        if role:
+            sql = "GRANT USAGE ON SCHEMA {} TO ROLE {}".format(target_schema, role)
+            self.query(sql)
+
+    def grant_select_on_schema(self, target_schema, role, to_group=False):
+        # Grant role is not mandatory parameter, do nothing if not specified
+        if role:
+            sql = "GRANT SELECT ON ALL TABLES IN SCHEMA {} TO ROLE {}".format(target_schema, role)
+            self.query(sql)
+
+    def obfuscate_columns(self, target_schema, table_name):
+        utils.log("SNOWFLAKE - Applying obfuscation rules")
+        table_dict = utils.tablename_to_dict(table_name)
+        temp_table = table_dict.get('temp_table_name')
+        transformations = self.transformation_config.get('transformations', [])
+        trans_cols = []
+
+        # Find obfuscation rule for the current table
+        for t in transformations:
+            # Input table_name is formatted as {{schema}}.{{table}}
+            # Stream name in taps transformation.json is formatted as {{schema}}-{{table}}
+            #
+            # We need to convert to the same format to find the transformation
+            # has that has to be applied
+            tap_stream_name_by_table_name = "{}-{}".format(table_dict['schema_name'], table_dict['table_name']) \
+                if table_dict['schema_name'] is not None else table_dict['table_name']
+
+            if t.get('tap_stream_name') == tap_stream_name_by_table_name:
+                # use safe field id in case the column to transform is has a name of a reserved word
+                # fallback to field_id if the safe id doesn't exist
+                column = t.get('safe_field_id', t.get('field_id'))
+                transform_type = t.get('type')
+                if transform_type == 'SET-NULL':
+                    trans_cols.append("{} = NULL".format(column))
+                elif transform_type == 'HASH':
+                    trans_cols.append("{} = SHA2({}, 256)".format(column, column))
+                elif 'HASH-SKIP-FIRST' in transform_type:
+                    skip_first_n = transform_type[-1]
+                    trans_cols.append(
+                        "{} = CONCAT(SUBSTRING({}, 1, {}), SHA2(SUBSTRING({}, {} + 1), 256))".format(column, column,
+                                                                                                     skip_first_n,
+                                                                                                     column,
+                                                                                                     skip_first_n))
+                elif transform_type == 'MASK-DATE':
+                    trans_cols.append("{} = TO_CHAR({}::DATE,'YYYY-01-01')::DATE".format(column, column))
+                elif transform_type == 'MASK-NUMBER':
+                    trans_cols.append("{} = 0".format(column))
+
+        # Generate and run UPDATE if at least one obfuscation rule found
+        if len(trans_cols) > 0:
+            sql = "UPDATE {}.{} SET {}".format(target_schema, temp_table, ','.join(trans_cols))
+            self.query(sql)
+
+    def swap_tables(self, schema, table_name):
+        table_dict = utils.tablename_to_dict(table_name)
+        target_table = table_dict.get('table_name')
+        temp_table = table_dict.get('temp_table_name')
+
+        # Swap tables and drop the temp tamp
+        self.query("ALTER TABLE {}.{} SWAP WITH {}.{}".format(schema, temp_table, schema, target_table))
+        self.query("DROP TABLE IF EXISTS {}.{}".format(schema, temp_table))
+
+    def cache_information_schema_columns(self, tables):
+        pipelinewise_schema = utils.tablename_to_dict(self.connection_config['stage'])['schema_name']
+        schemas_to_cache = utils.get_target_schemas(self.connection_config, tables)
+
+        # Create an empty cache table if not exists
+        self.query("""
+            CREATE TABLE IF NOT EXISTS {}.columns (table_schema VARCHAR, table_name VARCHAR, column_name VARCHAR, data_type VARCHAR)
+        """.format(pipelinewise_schema))
+
+        #  Cache table columns from information_schema
+        for schema_name in schemas_to_cache:
+            utils.log("rebuilding information_schema cache for schema: {}".format(schemas_to_cache))
+
+            # Delete existing data about the current schema
+            self.query("""
+                DELETE FROM {}.columns
+                WHERE LOWER(table_schema) = '{}'
+            """.format(pipelinewise_schema, schema_name.lower()))
+
+            # Insert the latest data from information_schema into the cache table
+            self.query("""
+                INSERT INTO {}.columns
+                SELECT table_schema, table_name, column_name, data_type
+                FROM information_schema.columns
+                WHERE LOWER(table_schema) = '{}'
+            """.format(pipelinewise_schema, schema_name.lower()))
